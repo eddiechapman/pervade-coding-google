e7e205cbe1524685c9e10f3bb7973b7b27536a11
Eddie <eddiechapman@users.noreply.github.com>
2018-07-30 10:57:05 -0500

    corrected "routes" to "views"

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')     # Public config
 app.config.from_pyfile('config.py')  # Instance folder config
 
-from app import routes
+from app import views
