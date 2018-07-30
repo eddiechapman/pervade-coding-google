661710fa478bf121a91a3ccd01ff5290cef721ee
Eddie <eddiechapman@users.noreply.github.com>
2018-07-30 10:25:39 -0500

    Flask object import, routes import

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+from flask import Flask
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')     # Public config
 app.config.from_pyfile('config.py')  # Instance folder config
+
+from app import routes
