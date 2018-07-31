044dbecda049db7596172f7d23a50bd734fbf0e0
Eddie <eddiechapman@users.noreply.github.com>
2018-07-31 13:34:18 -0500

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+
+class LoginForm(FlaskForm):
+    pervasive_data = BooleanField('Pervasive data')
+    data_science = BooleanField('Data science')
+    big_data = BooleanField('Big data')
+    case_study = BooleanField('Flag for PRIM&R case study')
+    data_synonyms = StringField('Data synonyms')
+    submit = SubmitField('Save')
