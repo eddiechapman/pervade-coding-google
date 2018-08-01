3531763ee745d603314ca36afadbea04846a25f0
Eddie <eddiechapman@users.noreply.github.com>
2018-08-01 13:06:41 -0500

    Added 'MAJOR_DIMENSION'

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ DEBUG = True
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '11nf3AlDsj_E53rlmReC1cq4nevIg8quGpn46tR2MwSs'
 HEADER_ROW = 'Sheet1!A1:N1'
+MAJOR_DIMENSION = 'ROWS'
 API_SERVICE_NAME = 'sheets'
 API_VERSION = 'v4'
 CLIENT_SECRETS_FILE = 'client_secret.json'
