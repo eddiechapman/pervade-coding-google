648601bddb0043c8006c75a91a8b7b29453c6aef
Eddie <eddiechapman@users.noreply.github.com>
2018-08-01 12:41:03 -0500

    Added SPREADSHEET_ID and HEADER_ROW values

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 DEBUG = True
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SPREADSHEET_ID = '11nf3AlDsj_E53rlmReC1cq4nevIg8quGpn46tR2MwSs'
+HEADER_ROW = 'Sheet1!A1:N1'
 API_SERVICE_NAME = 'sheets'
 API_VERSION = 'v4'
 CLIENT_SECRETS_FILE = 'client_secret.json'
