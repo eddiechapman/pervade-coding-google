1957c96adb2e4577d8a67882ee13cb8e593acf83
Eddie <eddiechapman@users.noreply.github.com>
2018-08-05 16:08:15 -0500

    removed instance config in favor of heroku variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_object('config')     # Public config
-app.config.from_pyfile('config.py')  # Instance folder config
 
 bootstrap = Bootstrap(app)
 
